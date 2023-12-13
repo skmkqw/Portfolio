7f2783ea1dc8c2ebb447fc2924d770cace398318
skmkqw <tkorsakov77@gmail.com>
2023-12-13 22:27:09 +0100

    Header + first section

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector('.menu-body')
+const menuBtn = document.querySelector('.menu-icon')
+const body = document.querySelector('body')
+
+if (menu && menuBtn) {
+	menuBtn.addEventListener('click', function () {
+		menu.classList.toggle('active')
+		menuBtn.classList.toggle('active')
+		body.classList.toggle('lock')
+	})
+
+	menu.querySelectorAll('.menu-link').forEach(link => {
+		link.addEventListener('click', function () {
+			menu.classList.remove('active')
+			menuBtn.classList.remove('active')
+			body.classList.remove('lock')
+		})
+	});
+}
