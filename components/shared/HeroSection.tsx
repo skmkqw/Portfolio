8443fe30c756e2f0e5542abb55c10d25b57c69af
skmkqw <tkorsakov77@gmail.com>
2024-08-14 21:34:19 +0200

    HeroSection style fix

--- components/shared/HeroSection.tsx
+++ components/shared/HeroSection.tsx
@@ -4,11 +4,18 @@ import Link from "next/link";
 
 export default function HeroSection() {
     return (
-        <Container className="mt-28 flex flex-col items-center text-center">
-            <Image src="/emoji.png" alt="Portrait emoji" width={200} height={200}
-                   className="bg-gradient-to-t from-indigo-500 to-orange-500 rounded-full" />
-            <Title text="Timofei Korsakov" className="mt-12 text-4xl md:text-5xl lg:text-6xl" />
-            <p className="font-semibold text-[#C9C9C9] text-xl md:text-2xl lg:text-3xl mt-4">
+        <Container className="mt-10 xs:mt-16 sm:mt-20 md:mt-28 flex flex-col items-center text-center">
+            <Image src="/emoji.png"
+                   alt="Portrait emoji"
+                   width={200}
+                   height={200}
+                   className="bg-gradient-to-t from-indigo-500 to-orange-500 rounded-full"
+            />
+            <Title
+                text="Timofei Korsakov"
+                className="mt-12"
+            />
+            <p className="font-semibold text-[#C9C9C9] text-2xl lg:text-3xl mt-4">
                 I do <span
                         className="bg-gradient-to-r from-indigo-500 to-orange-500 bg-clip-text text-transparent">
                     Code
@@ -16,10 +23,14 @@ export default function HeroSection() {
             </p>
             <Subtitle
                 text="Passionate Software Engineer with a focus on React + .NET fullstack development, dedicated to crafting elegant and user-friendly web applications."
-                className="max-w-xl mt-8 text-base md:text-lg" />
+                className="max-w-xl mt-8"
+            />
             <Link href="/contact">
-                <Button type="button" text="Contact me"
-                        additionalStyles="border border-gray-400 text-white !bg-black hover:!bg-gray-900 mt-9 py-3 px-12 rounded-full text-lg" />
+                <Button
+                    type="button"
+                    text="Contact me"
+                    additionalStyles="border border-gray-400 text-white !bg-black hover:!bg-gray-900 mt-9 py-3 px-12 rounded-full text-lg"
+                />
             </Link>
         </Container>
     );
