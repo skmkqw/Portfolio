78b3288e5cfb6855b724d3076b8c3f8f1109acab
skmkqw <tkorsakov77@gmail.com>
2024-08-14 15:39:52 +0200

    logo link

--- components/shared/Logo.tsx
+++ components/shared/Logo.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Logo() {
     return (
-        <div className={'flex gap-4 align-middle'}>
-            <Image src="/logo.svg" alt="Logo image" width={24} height={27}/>
-            <div className={'text-right'}>
-                <b className={'font-bold text-2xl'}>Timofei</b>
-                <p className={'text-xs tracking-widest'}>KORSAKOV</p>
+        <Link href="/">
+            <div className="flex gap-4 align-middle cursor-pointer">
+                <Image src="/logo.svg" alt="Logo image" width={24} height={27} />
+                <div className={"text-right"}>
+                    <b className={"font-bold text-2xl"}>Timofei</b>
+                    <p className={"text-xs tracking-widest"}>KORSAKOV</p>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 }
