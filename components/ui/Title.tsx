04ca612f544dfe2e32273b6549037dcd3a5f979a
skmkqw <tkorsakov77@gmail.com>
2024-08-14 15:40:54 +0200

    Title fix

--- components/ui/Title.tsx
+++ components/ui/Title.tsx
@@ -1,3 +1,3 @@
 export default function Title({ text, className }: { text: string, className?: string }) {
-    return <h1 className={`font-bold text-6xl ${className}`}>{text}</h1>;
+    return <h1 className={`${className} font-bold text-6xl`}>{text}</h1>;
 }
