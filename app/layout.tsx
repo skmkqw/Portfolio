cbe743c5d55e0861201638a5274a5a5772419e85
skmkqw <tkorsakov77@gmail.com>
2024-08-14 13:51:09 +0200

    added font sizes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
     subsets: ["latin"],
-    weight: ["400", "500"]
+    weight: ["400", "500", "600", "800"]
 })
 
 export default function RootLayout({
