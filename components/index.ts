f5c1590201ba431213dacd6ca01fee741f7b0db6
skmkqw <tkorsakov77@gmail.com>
2024-08-14 13:50:42 +0200

    updated index

--- components/index.ts
+++ components/index.ts
@@ -4,13 +4,16 @@ import Button from "@/components/ui/Button";
 import Header from "@/components/shared/Header";
 import Subtitle from "@/components/ui/Subtitle";
 import Footer from "@/components/shared/Footer";
+import Project from "@/components/shared/Project";
 import Container from "@/components/shared/Container";
 import Background from "@/components/shared/Background";
+import StackBadge from "@/components/shared/StackBadge";
 import GradientText from "@/components/ui/GradientText";
 import SocialIcons from "@/components/shared/SocialIcons";
 import HeroSection from "@/components/shared/HeroSection";
 import AboutSection from "@/components/shared/AboutSection";
 import StackSection from "@/components/shared/StackSection";
+import ProjectsSection from "@/components/shared/ProjectsSection";
 
 export {
     Header,
@@ -20,10 +23,13 @@ export {
     Background,
     SocialIcons,
     Button,
+    Project,
     HeroSection,
     Container,
     Subtitle,
     GradientText,
+    StackBadge,
     AboutSection,
-    StackSection
+    StackSection,
+    ProjectsSection
 };
