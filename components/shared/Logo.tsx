4f94fced74d413b1dbb4d2233bb8e47593bf9ad1
skmkqw <tkorsakov77@gmail.com>
2025-03-27 01:02:12 +0100

    change logo

--- components/shared/Logo.tsx
+++ components/shared/Logo.tsx
@@ -3,11 +3,8 @@ import Link from "next/link";
 export default function Logo() {
     return (
         <Link href="/">
-            <div className="flex gap-4 items-center cursor-pointer">
-                <div className={"text-right"}>
-                    <b className={"font-bold text-2xl"}>Timofei</b>
-                    <p className={"text-xs tracking-widest"}>KORSAKOV</p>
-                </div>
+            <div className="cursor-pointer">
+                <p className={"text-2xl tracking-wide"}>{"<skmkqw/>"}</p>
             </div>
         </Link>
     );
